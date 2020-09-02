ae5446f12edde8ecd8fd27cb3dfe3ea935ac2b35
ThejusS <thejuspayikkattu@gmail.com>
2020-09-02 20:40:17 +0530

    project page phase 1, loader, some fixes

--- resourses/js/script.js
+++ resourses/js/script.js
@@ -1,4 +1,40 @@
 $(document).ready(function(){
+
+  // Disable right click
+  // document.onmousedown=disableclick;
+  // status="Sorry dear... Right click is disabled";
+  // function disableclick(event){
+  //   if(event.button ==2){
+  //     alert(status);
+  //     return false;
+  //   }
+  // }
+
+  // Disable f12 and all other keys to inspect the source code
+  document.onkeydown =function(e){
+    if(event.keyCode == 123){
+      return false;
+    }
+    if(e.ctrlKey && e.shiftKey && (e.keyCode == 'I'.charCodeAt(0) || e.keyCode == 'i'.charCodeAt(0))){
+      return false;
+    }
+    if (e.ctrlKey && e.shiftKey && (e.keyCode == 'C'.charCodeAt(0) || e.keyCode == 'c'.charCodeAt(0))) {
+      return false;
+    }
+    if (e.ctrlKey && e.shiftKey && (e.keyCode == 'J'.charCodeAt(0) || e.keyCode == 'j'.charCodeAt(0))) {
+      return false;
+    } 
+    if (e.ctrlKey && (e.keyCode == 'U'.charCodeAt(0) || e.keyCode == 'u'.charCodeAt(0))) {
+      codeMsg = "Are you trying to read my code ?!"
+      alert(codeMsg);
+      return false;
+    }
+    if (e.ctrlKey && (e.keyCode == 'S'.charCodeAt(0) || e.keyCode == 's'.charCodeAt(0))) {
+      saveMsg = "No no no... You can't save this website"
+      alert(saveMsg)
+      return false;
+    }
+  }
     
   //  $('h1').click(function(){
   //      $(this).css('background-color','#ff0000');
@@ -8,23 +44,67 @@ $(document).ready(function(){
   //      $(this).css('background-color','#ff0000');
   //  })
    
+  // Mobile Navigation
   const hamburger = document.querySelector('.hamburger');
   const navLinks = document.querySelector('.nav-links');
   const links = document.querySelectorAll('nav-links li');
 
   hamburger.addEventListener('click', () => {
     navLinks.classList.toggle("open");
-    // links.forEach(link =>{
-    //   link.classList.toggle("fade");
-    // });
-    // links.classList.toggle("fade");
   });
 
-  // window.addEventListener("scroll", function(){
-  //   var nav = document.querySelector("nav");
-  //   nav.classList.toggle("sticky", window.scrollY);
-  // })
-    
   
+  // Loading screen animation
+  setTimeout(function(){
+    $('.loader_bg').fadeToggle();
+  },1500);
+
+  // Element filter in projects page
+  const buttons = document.querySelector(".project-links").children;
+  const items = document.querySelector(".scrollContent").children;
+  
+  for (let i=0; i<buttons.length; i++){
+    buttons[i].addEventListener("click",function(){
+      for(let j=0; j<buttons.length; j++){
+        buttons[j].classList.remove("active");
+      }
+      this.classList.add("active");
+      const target = this.getAttribute("data-target");
+
+      for(let k=0; k<items.length; k++){
+        items[k].style.display="none";
+
+        if(items[k].getAttribute("data-id") == target){
+          items[k].style.display = "block";
+        }
+        if (target == "all") {
+          items[k].style.display = "block";
+        }
+      }
+    })
+  }
+
+  // Specific project display
+  const cards = document.querySelector(".scrollContent").children;
+  const displayItems = document.querySelector(".mainDisplay").children;
+  
+
+  for(let x=0; x<cards.length; x++){
+    cards[x].addEventListener("click", function(){
+
+      const cardValue = this.getAttribute("data-card");
+
+      for(let y=0; y,displayItems.length; y++){
+        displayItems[y].style.display="none";
+
+        if(displayItems[y].getAttribute("data-show") == cardValue){
+          displayItems[y].style.display = "block";
+        }
+      }
+    })
+  }
+
+
+
 });
 
